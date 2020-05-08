63d8663522bb60dafa87005fe2b59fd2d5276c3d
Kr1t1ka <poloposhta@gmail.com>
2020-05-08 14:07:35 +0400

    готово

--- src/mainApp.kt
+++ src/mainApp.kt
@@ -4,48 +4,40 @@ fun main(){
     var a: Double
     var b: Double
     var c: Double
-    var d: Double
-    var menu = true
-    while (menu) {
-        while (true) {
-            try {
-                println("Введите кофицент а, b, c через пробел: ")
-                val (a1: Double, b1: Double, c1: Double) = readLine()!!.split(' ').map(String::toDouble)
-                a = a1
-                b = b1
-                c = c1
-                break
-            } catch (e: Exception) {
-                println("ОШИБКА")
-            }
+    val d: Double
+    while (true) {
+        try {
+            println("Введите коэффициенты а, b, c через пробел: ")
+            val (a1: Double, b1: Double, c1: Double) = readLine()!!.split(' ').map(String::toDouble)
+            a = a1
+            b = b1
+            c = c1
+            break
+        } catch (e: Exception) {
+            println("ОШИБКА: недопустимые значения")
         }
-        if (a == 0.0 && b == 0.0) {
-            if (c == 0.0) {
-                println("x - может быть любым числом")
-            } else {
-                println("x - может быть любым числом")
-            }
-        } else if (a == 0.0) {
-            println("x1 = ${-c / b}")
+    }
+    if (a == 0.0 && b == 0.0) {
+        if (c == 0.0) {
+            println("x - может быть любым числом")
         } else {
-            d = b.pow(2.0) - 4 * a * c
-            when {
-                d > 0 -> {
-                    println("x1 = ${(-b + sqrt(d)) / (2 * a)}")
-                    println("x2 = ${(-b - sqrt(d)) / (2 * a)}")
-                }
-                d == 0.0 -> {
-                    println("x1 = ${(-b) / 2 * a}")
-                }
-                else -> {
-                    println("Уровнение не имеет действительных корней")
-                }
-            }
+            println("Уравнение не имеет действительных корней")
         }
-        println("Продолжить? Введите (Н) для остановки программы.")
-        val tmp = readLine()
-        if (tmp == "Н" || tmp == "н"){
-            menu = false
+    } else if (a == 0.0) {
+        println("x1 = ${-c / b}")
+    } else {
+        d = b.pow(2.0) - 4 * a * c
+        when {
+            d > 0 -> {
+                println("x1 = ${(-b + sqrt(d)) / (2 * a)}")
+                println("x2 = ${(-b - sqrt(d)) / (2 * a)}")
+            }
+            d == 0.0 -> {
+                println("x1 = ${(-b) / (2 * a)}")
+            }
+            else -> {
+                println("Уравнение не имеет действительных корней")
+            }
         }
     }
 }
